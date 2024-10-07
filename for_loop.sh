e1b0c67fc5f81f223f587c86f4173350f677d2d8
Alucard Stormbringer <supersayan1000@gmail.com>
2024-10-07 15:11:54 +0530

    Add Loop Examples

---
+++ for_loop.sh
@@ -0,0 +1,6 @@
+# For Loop Example
+
+for ((i=0; i<=10; i++))
+do
+echo "loop ran $i time"
+done
---
+++ while_loop.sh
@@ -0,0 +1,8 @@
+# While Loop Example
+
+i=1
+while [ $i -le 10 ]
+do
+echo "Loop ran $i times"
+((i++))
+done
